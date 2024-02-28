2d244b49464ddd97b438a2cccde1e6ff55bbe746
Yhwani <yh8805@naver.com>
2024-02-28 16:07:12 +0900

    first commit

---
+++ src/main/java/yeonghwan/project/springmodulebase/SpringModuleBaseApplication.java
@@ -0,0 +1,13 @@
+package yeonghwan.project.springmodulebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringModuleBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringModuleBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yeonghwan/project/springmodulebase/SpringModuleBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package yeonghwan.project.springmodulebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringModuleBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
